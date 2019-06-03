e9825f82605f1d07098e8a3e02b8e903070c1088
Caleb Scheidel <caleb@methodsconsultants.com>
2019-06-03 15:19:42 -0400

    use five variables in academic measurement model for university prestige dataset

--- university-prestige/university-prestige.R
+++ university-prestige/university-prestige.R
@@ -4,12 +4,12 @@ set.seed(1)
 
 # specify model
 population.model <- '
-  acad  =~ acad1 + 0.85*acad2 + 1.25*acad3
-  alum  =~ alum1 + 0.6*alum2 + 1.4*alum3
-  extra =~ extra1 + 0.25*extra2 + 0.8*extra3
+  acad  =~ acad1 + 0.85*acad2 + 9*acad3 + 0.8*acad4 + 0.95*acad5
+  alum  =~ alum1 + 0.8*alum2 + 0.82*alum3
+  extra =~ extra1 + 0.9*extra2 + 0.95*extra3
 
-  alum  ~ 0.66*acad
-  extra ~ 0.52*acad + 0.61*alum
+  alum  ~ 0.8*acad
+  extra ~ 0.875*acad + 0.925*alum
 '
 
 # generate data
