da99d1c67ff03a66b0b4c226b8ecad2ad67229c4
Caleb Scheidel <caleb@methodsconsultants.com>
2019-06-03 15:46:52 -0400

    adjust coefs to between .8 and 1 when simulating univ prestige data

--- university-prestige/university-prestige.R
+++ university-prestige/university-prestige.R
@@ -4,7 +4,7 @@ set.seed(1)
 
 # specify model
 population.model <- '
-  acad  =~ acad1 + 0.85*acad2 + 9*acad3 + 0.8*acad4 + 0.95*acad5
+  acad  =~ acad1 + 0.85*acad2 + .9*acad3 + 0.8*acad4 + 0.95*acad5
   alum  =~ alum1 + 0.8*alum2 + 0.82*alum3
   extra =~ extra1 + 0.9*extra2 + 0.95*extra3
 
